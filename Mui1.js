cdfbec3963ef5be759970f7bafe041195f575475
tarannumbeniwale031019 <159605766+tarannumbeniwale031019@users.noreply.github.com>
2024-04-08 10:23:17 +0530

    Add files via upload

---
+++ Mui1.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import Switch from '@mui/material/Switch';
+
+export default function Sw() {
+  const [checked, setChecked] = React.useState(true);
+
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
+
+  return (<>
+    <Switch
+      checked={checked}
+      onChange={handleChange}
+      inputProps={{ 'aria-label': 'controlled' }}
+    />
+    <div>{""+checked}</div>
+
+</>
+  );
+}
